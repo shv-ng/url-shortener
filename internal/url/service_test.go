a8df13b3b68d9040d3232238b93d7b7961028a68
ShivangSrivastava <shivangsrivastava157@gmail.com>
2025-05-21 08:33:30 +0530

    Refactor service test

--- internal/url/service_test.go
+++ internal/url/service_test.go
@@ -7,12 +7,9 @@ import (
 
 func TestGenerateShortURL(t *testing.T) {
 	original_url := "https://google.com"
-	expected := Url{
-		OriginalURL: original_url,
-		ShortURL:    "99999ebc",
-	}
+	expected := "99999ebc"
 	short_url := generateShortURL(original_url)
-	if !reflect.DeepEqual(short_url, expected.ShortURL) {
-		t.Errorf("Expected %v, got %v", expected.ShortURL, short_url)
+	if !reflect.DeepEqual(short_url, expected) {
+		t.Errorf("Expected %v, got %v", expected, short_url)
 	}
 }
